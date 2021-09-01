cfc1588dbb84db0a85119c9e6192c0ae19f14414
nbeckley23 <nickbeckley@gmail.com>
2021-09-01 11:37:19 -0600

    began landingpage layout and newUser layout as well as created interface for landingPageFragment

--- app/src/main/java/com/example/lyfr/MainActivity.kt
+++ app/src/main/java/com/example/lyfr/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.lyfr
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LandingPageExistingUsersFragment.DataPassingInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onDataPass(data: Array<String>) {
+        TODO("Not yet implemented")
     }
 }
