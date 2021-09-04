94ee0639ee574c3a10c0f1d4f821f8f5e13d163b
nbeckley23 <nickbeckley@gmail.com>
2021-09-04 12:39:02 -0600

    landing page and new user activity

--- app/src/main/java/com/example/lyfr/MainActivity.kt
+++ app/src/main/java/com/example/lyfr/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.example.lyfr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity(), LandingPageExistingUsersFragment.DataPassingInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.create_new_user)
+        button.setOnClickListener{
+            val intent = Intent(this, NewUserActivity::class.java).apply {
+            }
+            startActivity(intent)
+        }
     }
 
     override fun onDataPass(data: Array<String>) {
         TODO("Not yet implemented")
     }
+
+//     fun newUser(){
+//        setContentView(R.layout.activity_new_user)
+//    }
 }
