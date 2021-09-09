940e066295c6c1b420c8b2227dc0518ccb6f0b53
Taylor <taylorjodunn@gmail.com>
2021-09-08 20:06:48 -0600

    refactor(User) change user to have getters and setters vs. input parameters for construction. Added blue borders to text views on new user activity.

--- app/src/main/java/User.kt
+++ app/src/main/java/User.kt
@@ -1,20 +1,8 @@
-class User (val name: String,
-            val city: String,
-            val country: String,
-            val sex: String,
-            val ageStr: String,
-            val heightStr: String,
-            val weightStr: String,
-){
-    val nameArray = name.split(" ").toTypedArray()
-    var fname : String = nameArray[0]
-    var lname : String = ""
-    var age: Int = ageStr.toInt()
-    var height: Double = heightStr.toDouble()
-    var weight: Double = weightStr.toDouble()
-
-    init {
-        if (nameArray.size > 1)
-            lname = nameArray[1]
-    }
+class User (){
+    var name : String = ""
+    var zip : String = ""
+    var age: Int = 0
+    var sex: String = ""
+    var height: Double = 0.0
+    var weight: Double = 0.0
 }
