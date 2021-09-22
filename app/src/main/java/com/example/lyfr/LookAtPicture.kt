3dc4ea0ff7c0861413e4eecc68594e1a2a161d03
Tyler <u1275144@utah.edu>
2021-09-22 01:52:48 -0600

    blank page after taking a photo, no more

--- app/src/main/java/com/example/lyfr/LookAtPicture.kt
+++ app/src/main/java/com/example/lyfr/LookAtPicture.kt
@@ -1,7 +1,10 @@
 package com.example.lyfr
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
 import com.example.lyfr.databinding.PictureLookAtBinding
 
 
@@ -11,7 +14,17 @@ class LookAtPicture : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.picture_look_at)
         binding = PictureLookAtBinding.inflate(layoutInflater)
+        setContentView(R.layout.picture_look_at)
+
+        val profilePicture by lazy { findViewById<ImageView>(R.id.profilePicture) }
+        profilePicture.setImageURI(null)
+        profilePicture.setImageURI(ImageUri.latestTmpUri)
+
+        profilePicture.setOnClickListener{
+            val editProfileIntent = Intent(this, NewUserActivity::class.java).apply {
+            }
+            startActivity(editProfileIntent)}
+
     }
 }
