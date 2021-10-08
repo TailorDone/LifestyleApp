b1491a6043c200d48c4de2755abba00136587f35
RClark4958 <rickdclark48@gmail.com>
2021-10-08 09:37:21 -0600

    update_rick_branch

--- app/src/androidTest/java/com/example/lyfr/TestsOnTestsOnTests.kt
+++ app/src/androidTest/java/com/example/lyfr/TestsOnTestsOnTests.kt
@@ -3,14 +3,8 @@ package com.example.lyfr
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
-import androidx.test.runner.AndroidJUnit4
 import org.hamcrest.Matchers.allOf
-import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
-
 
 class TestsOnTestsOnTests {
 
