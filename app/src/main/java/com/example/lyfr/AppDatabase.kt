90ebe728b7d5037462506f3f5b05e3d510753775
nickbeckley23 <nickbeckley@gmail.com>
2021-10-25 17:57:52 -0600

    added new version number for db and alowed fallbackToDestructiveMigration

--- app/src/main/java/com/example/lyfr/AppDatabase.kt
+++ app/src/main/java/com/example/lyfr/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [User::class], version = 1, exportSchema = false)
+@Database(entities = [User::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun dao(): DAO
@@ -24,7 +24,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "com.example.lyfr.AppDatabase"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
                 //.allowMainThreadQueries()
                 INSTANCE = instance
                 // return instance
