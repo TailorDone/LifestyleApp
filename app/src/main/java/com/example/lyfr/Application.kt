729adf8df284dc1ac9b1d1a1c95a9a5305932637
Rick Rick <rickdclark48@gmail.com>
2021-10-31 15:29:19 -0600

    amplify_storage_and_auth

--- app/src/main/java/com/example/lyfr/Application.kt
+++ app/src/main/java/com/example/lyfr/Application.kt
@@ -3,7 +3,9 @@ package com.example.lyfr
 import android.app.Application
 import android.util.Log
 import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
 import com.amplifyframework.core.Amplify
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin
 
 class LYFR_Application : Application() {
     // Using by lazy so the database and the repository are only created when they're needed
@@ -15,6 +17,8 @@ class LYFR_Application : Application() {
         super.onCreate()
 
         try {
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+            Amplify.addPlugin(AWSS3StoragePlugin())
             Amplify.configure(applicationContext)
             Log.i("LYFR_Application", "Initialized Amplify")
         } catch (error: AmplifyException) {
